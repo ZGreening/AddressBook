0ffcb92b915770ba051c2053a62bdc7e0e444691
ZGreening <42820945+ZGreening@users.noreply.github.com>
2020-04-21 11:40:14 -0400

    Formatted Tests.

--- tests/AddressBookControllerTest.java
+++ tests/AddressBookControllerTest.java
@@ -6,8 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.Rule;
 import org.junit.jupiter.api.AfterEach;
@@ -16,189 +14,176 @@ import org.junit.jupiter.api.Test;
 import org.junit.rules.TemporaryFolder;
 
 /**
-    * AddressBookControllerTest.java
-    * This test classes contain AddressBook Controller unit tests
-    * This class uses JUnit as a test suite to execute tests.
-    */
+ * AddressBookControllerTest.java This test classes contain AddressBook
+ * Controller unit tests This class uses JUnit as a test suite to execute tests.
+ */
 class AddressBookControllerTest {
 
-  Person test_Person; // test person
-  AddressBook test_AddressBook; // test AddressBook
+    @Rule
+    public static TemporaryFolder folder = new TemporaryFolder();
+    private static File file = null; // A java file for testing
+    private static Person test_Person; // test person
+    private static AddressBook test_AddressBook; // test AddressBook
+    private static AddressBookController controllerTest; // Test Controls
 
-  AddressBookController controllerTest; //Test Controlls
+    /**
+     * This method is met to set up variables for test cases. This method is run
+     * before each test
+     */
+    @BeforeEach
+    void setUp() {
 
-  /**
-   * This method is met to set up variables for test cases. This method is run before each test
-   */
-  @BeforeEach
-  void setUp() {
-
-    //model = new DefaultTableModel();
-    test_Person = new Person("Jane", "Dorian",
-        "987 Westbrook Blvd", "Chincinnati", "OH", "43123", "0123456789");
-    test_AddressBook = new AddressBook();
-    controllerTest = new AddressBookController(test_AddressBook);
-  }
-
-  /**
-   * This method is ran after each test and clear out variables.
-   */
-  @AfterEach
-  void tearDown() {
-    test_Person = null;
-    test_AddressBook = null;
-    controllerTest = null;
-  }
-
-  /**
-   * This test case test will test if a person is successful added to Address Book.
-   */
-  @Test
-  void addPersonToList() {
-    controllerTest.add(test_Person);
-
-   assertEquals(controllerTest.get(0).getFirstName(), "Jane");
-  }
-
-  /**
-   * This case tests set method. Given an index a person object will be replaced with a new person
-   * object.
-   */
-  @Test
-  void setWithOnePersonInList() {
-    controllerTest.add(test_Person);
-    controllerTest.set(0, test_Person);
-
-    assertEquals(controllerTest.get(0), test_Person);
-    assertEquals("Dorian, Jane", controllerTest.get(0).toString());
-  }
-
-  /**
-   * This is a negative test case that will throw an Exception if the method tries to set a new
-   * person on a index that doesn't exist.
-   */
-  @Test
-  void setWithEmptyListShouldThrowError() {
-    assertThrows(IndexOutOfBoundsException.class, () -> controllerTest.set(0, test_Person));
-
-  }
-
-  /**
-   * This test case will test that a person will be removed from Address Book.
-   */
-  @Test
-  void removePersonFromListAndListWillNotBeEmpty() {
-    controllerTest.add(test_Person); //Add person in AddressBook
-
-    controllerTest.remove(0);//Remove Person
-    //Check if removed person is in the list
-    boolean isInArray = false;
-    String search = "Dorian, Jane";
-    for (int i = 0; i < test_AddressBook.getPersons().length; i++) {
-      if (test_AddressBook.getPersons()[i].toString().equals(search)) {
-        //Found Person!
-        isInArray = true;
-        break;
-      }
+        // model = new DefaultTableModel();
+        test_Person = new Person("Jane", "Dorian", "987 Westbrook Blvd", "Cincinnati", "OH", "43123", "0123456789");
+        test_AddressBook = new AddressBook();
+        controllerTest = new AddressBookController(test_AddressBook);
     }
-    //Assert Person not found
-    assertFalse(isInArray);
-    assertEquals(0,test_AddressBook.getPersons().length);
-  }
 
-  /**
-   * This is a negative test case to test that test that an Error is thrown if the method tries to
-   * remove a person from a empty Person list.
-   */
-  @Test
-  void removeOnEmptyListShouldThrowError() {
-    assertThrows(IndexOutOfBoundsException.class, () -> controllerTest.remove(0));
-  }
+    /**
+     * This method is ran after each test and clear out variables.
+     */
+    @AfterEach
+    void tearDown() {
+        test_Person = null;
+        test_AddressBook = null;
+        controllerTest = null;
+    }
 
-  /**
-   * This test case will test if AddressBook Controller will successfully add a person.
-   */
-  @Test
-  void getPersonOnNonEmptyList() {
-    controllerTest.add(test_Person);
-    assertEquals(controllerTest.get(0), test_Person);
+    /**
+     * This test case test will test if a person is successful added to Address
+     * Book.
+     */
+    @Test
+    void addPersonToList() {
+        controllerTest.add(test_Person);
+        assertEquals("Jane", controllerTest.get(0).getFirstName());
+    }
 
-  }
+    /**
+     * This case tests set method. Given an index a person object will be replaced
+     * with a new person object.
+     */
+    @Test
+    void setWithOnePersonInList() {
+        controllerTest.add(test_Person);
+        controllerTest.set(0, test_Person);
 
-  @Test
-  /**
-   * This is a exception test which should throw an error if get method
-   * is called when list is empty
-   */
-  void getOnEmptyListShouldThrowError() {
-    assertThrows(IndexOutOfBoundsException.class, () -> controllerTest.get(0));
-  }
+        assertEquals(controllerTest.get(0), test_Person);
+        assertEquals("Dorian, Jane", controllerTest.get(0).toString());
+    }
 
-  /**
-   * This test case test if Address Book list will clear, given there is a person already in the
-   * Address Book
-   */
-  @Test
-  void clearList() {
-    test_AddressBook.add(test_Person);
-    //Array should not be empty
-    assertFalse(test_AddressBook.getPersons().length == 0);
+    /**
+     * This is a negative test case that will throw an Exception if the method tries
+     * to set a new person on a index that doesn't exist.
+     */
+    @Test
+    void setWithEmptyListShouldThrowError() {
+        assertThrows(IndexOutOfBoundsException.class, () -> controllerTest.set(0, test_Person));
+    }
 
-    controllerTest.clear();//Clear AddressBook
-    //Assert its empty
-    assertTrue(test_AddressBook.getPersons().length == 0);
-    assertThrows(IndexOutOfBoundsException.class, () -> controllerTest.get(0));
-  }
+    /**
+     * This test case will test that a person will be removed from Address Book.
+     */
+    @Test
+    void removePersonFromListAndListWillNotBeEmpty() {
+        controllerTest.add(test_Person); // Add person in AddressBook
 
-  @Rule
-  public static TemporaryFolder folder = new TemporaryFolder();
-  private static FileSystem fileSystem = null; //The file system for testing
-  private static File file = null;           //A java file for testing
+        controllerTest.remove(0);// Remove Person
+        // Check if removed person is in the list
+        boolean isInArray = false;
+        String search = "Dorian, Jane";
+        for (int i = 0; i < test_AddressBook.getPersons().length; i++) {
+            if (test_AddressBook.getPersons()[i].toString().equals(search)) {
+                // Found Person!
+                isInArray = true;
+                break;
+            }
+        }
 
-  /**
-   * This test case will test if program can open a normal file. The testing of file system is for
-   * the FileSystem test class thus, the test here will be a simple case of seeing if a acceptable
-   * file will open.
-   */
-  @Test
-  public void openValidFile() throws IOException {
+        // Assert Person not found
+        assertFalse(isInArray);
+        assertEquals(0, test_AddressBook.getPersons().length);
+    }
 
-    //Create a new file for testing each time
-    folder.create();
-    file = folder.newFile("MyTestFile");
+    /**
+     * This is a negative test case to test that test that an Error is thrown if the
+     * method tries to remove a person from a empty Person list.
+     */
+    @Test
+    void removeOnEmptyListShouldThrowError() {
+        assertThrows(IndexOutOfBoundsException.class, () -> controllerTest.remove(0));
+    }
 
-    //Add two people to address book and save
-    test_AddressBook.add(
-        new Person("John", "Doe", "1234 NON EXIST ROAD", "SomeCity", "FL", "12345", "1234567890"));
-    test_AddressBook.add(
-        new Person("Jane", "Doe", "1234 NON EXIST ROAD", "SomeCity", "FL", "12345", "1234567890"));
-    assertDoesNotThrow(() -> FileSystem.saveFile(test_AddressBook, file));
-    assertDoesNotThrow(() -> controllerTest.open(file));
-  }
+    /**
+     * This test case will test if AddressBook Controller will successfully add a
+     * person.
+     */
+    @Test
+    void getPersonOnNonEmptyList() {
+        controllerTest.add(test_Person);
+        assertEquals(controllerTest.get(0), test_Person);
+    }
 
-  /**
-   * This testcase will check if program will be able to successfully save a file.
-   */
-  @Test
-  public void saveValidFile() throws IOException {
+    /**
+     * This is a exception test which should throw an error if get method is called
+     * when list is empty
+     */
+    @Test
+    void getOnEmptyListShouldThrowError() {
+        assertThrows(IndexOutOfBoundsException.class, () -> controllerTest.get(0));
+    }
 
-    //Create a new file for testing each time
-    folder.create();
-    file = folder.newFile("MyTestFile");
+    /**
+     * This test case test if Address Book list will clear, given there is a person
+     * already in the Address Book
+     */
+    @Test
+    void clearList() {
+        test_AddressBook.add(test_Person);
+        // Array should not be empty
+        assertFalse(test_AddressBook.getPersons().length == 0);
 
-    //Add two people to address book
-    test_AddressBook.add(
-        new Person("John", "Doe", "1234 NON EXIST ROAD", "SomeCity", "FL", "12345", "1234567890"));
-    test_AddressBook.add(
-        new Person("Jane", "Doe", "1234 NON EXIST ROAD", "SomeCity", "FL", "12345", "1234567890"));
+        controllerTest.clear();// Clear AddressBook
+        // Assert its empty
+        assertTrue(test_AddressBook.getPersons().length == 0);
+        assertThrows(IndexOutOfBoundsException.class, () -> controllerTest.get(0));
+    }
 
-    assertDoesNotThrow(() -> controllerTest.save(file));
-    assertTrue(file.exists()); //Check file exists on disk
-  }
+    /**
+     * This test case will test if program can open a normal file. The testing of
+     * file system is for the FileSystem test class thus, the test here will be a
+     * simple case of seeing if a acceptable file will open.
+     */
+    @Test
+    public void openValidFile() throws IOException {
 
+        // Create a new file for testing each time
+        folder.create();
+        file = folder.newFile("MyTestFile");
+
+        // Add two people to address book and save
+        test_AddressBook.add(new Person("John", "Doe", "1234 NON EXIST ROAD", "SomeCity", "FL", "12345", "1234567890"));
+        test_AddressBook.add(new Person("Jane", "Doe", "1234 NON EXIST ROAD", "SomeCity", "FL", "12345", "1234567890"));
+        assertDoesNotThrow(() -> FileSystem.saveFile(test_AddressBook, file));
+        assertDoesNotThrow(() -> controllerTest.open(file));
+    }
+
+    /**
+     * This test case will check if program will be able to successfully save a
+     * file.
+     */
+    @Test
+    public void saveValidFile() throws IOException {
+
+        // Create a new file for testing each time
+        folder.create();
+        file = folder.newFile("MyTestFile");
+
+        // Add two people to address book
+        test_AddressBook.add(new Person("John", "Doe", "1234 NON EXIST ROAD", "SomeCity", "FL", "12345", "1234567890"));
+        test_AddressBook.add(new Person("Jane", "Doe", "1234 NON EXIST ROAD", "SomeCity", "FL", "12345", "1234567890"));
+
+        assertDoesNotThrow(() -> controllerTest.save(file));
+        assertTrue(file.exists()); // Check file exists on disk
+    }
 }
-
-
-
-
-
--- tests/AddressBookIntegrationTest.java
+++ tests/AddressBookIntegrationTest.java
@@ -5,9 +5,6 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.List;
-import javax.swing.table.DefaultTableModel;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,71 +12,64 @@ import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 /**
- * AddressBookIntegrationTest.java
- * This TestClass will run intergration tests for AddressBook class
- * This classes uses Mocktio to run mock and stub tests.
- * The Person class is mocked as AddressBook interacts with that class.
- *
+ * AddressBookIntegrationTest.java This TestClass will run integration tests for
+ * AddressBook class This classes uses Mockito to run mock and stub tests. The
+ * Person class is mocked as AddressBook interacts with that class.
  */
 @RunWith(MockitoJUnitRunner.class)
 class AddressBookIntegrationTest {
 
-  Person test_Person; //Test Person
-  AddressBook test_AddressBook; //Test AddressBook
-  Person person_mock; //Person Mock
-  Person person_spy;// person spy
+    Person test_Person; // Test Person
+    AddressBook test_AddressBook; // Test AddressBook
+    Person person_mock; // Person Mock
+    Person person_spy;// person spy
 
+    @BeforeEach
+    /**
+     * This method runs first before each test.
+     */
+    void setUp() {
+        test_Person = new Person("Jane", "Dorian", "987 Westbrook Blvd", "Cincinnati", "OH", "43123", "0123456789");
+        test_AddressBook = new AddressBook();
+        person_spy = spy(test_Person);
+        person_mock = mock(Person.class);
 
-  @BeforeEach
-  /**
-   * This method runs first before each test.
-   */
-  void setUp() {
-    test_Person = new Person("Jane","Dorian",
-        "987 Westbrook Blvd","Chincinnati","OH","43123","0123456789");
-    test_AddressBook = new AddressBook();
-    person_spy = spy(test_Person);
-    person_mock = mock(Person.class);
+    }
 
-  }
+    /**
+     * This method runs after each test, to clear out variables.
+     */
+    @AfterEach
+    void tearDown() {
+        test_Person = null;
+        test_AddressBook = null;
+        person_mock = null;
+        person_spy = null;
+    }
 
-  /**
-   * This method runs after each test, to clear out variables.
-   */
-  @AfterEach
-  void tearDown() {
-    test_Person=null;
-    test_AddressBook=null;
-    person_mock=null;
-    person_spy=null;
-  }
+    @Test
+    /**
+     * This test case is a stub will use a Person mock to force a custom return
+     * value.
+     */
+    public void getValueAtStub() {
+        test_AddressBook.add(person_mock);
+        when(person_mock.getField(0)).thenReturn("Sheldon");
+        test_AddressBook.getValueAt(0, 0);
+        verify(person_mock).getField(isA(int.class));
+        assertEquals("Sheldon", test_AddressBook.getValueAt(0, 0));
+    }
 
-  @Test
-  /**
-   * This testcase is a stub will use a Person mock
-   * to force a custom return value.
-   */
-  public void getValueAtStub(){
-    test_AddressBook.add(person_mock);
-    when(person_mock.getField(0)).thenReturn("Sheldon");
-    test_AddressBook.getValueAt(0,0);
-    verify(person_mock).getField(isA(int.class));
-    assertEquals("Sheldon",test_AddressBook.getValueAt
-        (0,0));
-  }
-
-  /**
-   * This test will use a spy of Person class to test that
-   * correct value is retrieved from person
-   */
-  @Test
-  public void getValueAtSpy(){
-    test_AddressBook.add(person_spy);
-    test_AddressBook.getValueAt(0,0);
-    verify(person_spy).getField(isA(int.class));
-    assertEquals("Dorian",test_AddressBook.getValueAt(0,0));
-    assertEquals("Jane",test_AddressBook.getValueAt(0,1));
-  }
-
-
+    /**
+     * This test will use a spy of Person class to test that correct value is
+     * retrieved from person
+     */
+    @Test
+    public void getValueAtSpy() {
+        test_AddressBook.add(person_spy);
+        test_AddressBook.getValueAt(0, 0);
+        verify(person_spy).getField(isA(int.class));
+        assertEquals("Dorian", test_AddressBook.getValueAt(0, 0));
+        assertEquals("Jane", test_AddressBook.getValueAt(0, 1));
+    }
 }
